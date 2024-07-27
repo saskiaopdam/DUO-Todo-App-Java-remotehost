ba24587822b0715f00085c22bedf66637a1dda4d
Saskia Opdam <opdamsaskia@icloud.com>
2024-07-27 11:50:27 +0200

    Add HomeController (GET "/" endpoint and mapping)

---
+++ src/main/java/com/example/todo/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.todo.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "Welcome to the Todo App";
+    }
+}
