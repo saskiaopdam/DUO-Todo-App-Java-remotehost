afc2f3a967e523b3b72a8389c98826a41494f8f0
Saskia Opdam <opdamsaskia@icloud.com>
2024-07-27 13:21:21 +0200

    Update CorsConfiguration.java - undo changes

--- src/main/java/com/example/todo/CorsConfiguration.java
+++ src/main/java/com/example/todo/CorsConfiguration.java
@@ -1,39 +1,16 @@
 package com.example.todo;
 
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-// @Configuration
-// @EnableWebMvc
-// public class CorsConfiguration implements WebMvcConfigurer {
-
-//     @Override
-//     public void addCorsMappings(CorsRegistry registry) {
-//         registry.addMapping("/**").allowedMethods("GET", "POST","PUT", "DELETE");
-//     }
-// }
-
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("https://duo-todo-app-angular-88e1837166f5.herokuapp.com")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET", "POST","PUT", "DELETE");
     }
 }
